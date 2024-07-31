e1fabf55a99a4d8d409981c9b835327615747afe
Grikh-UU-Py <5186415@mail.ru>
2024-07-31 15:32:41 +0300

    Update module_1_2.py

--- module_1_2.py
+++ module_1_2.py
@@ -2,4 +2,4 @@ done_HomeWork = 12
 spent_hours = 1.5
 name_course = 'Python'
 time_task = spent_hours / done_HomeWork
-print('Курс:', name_course, ", всего задач:",done_HomeWork,', затрачено часов:',spent_hours,', среднее время выполенния',time_task,'часа')
+print('Курс:', name_course, ", всего задач:",done_HomeWork,', затрачено часов:',spent_hours,', среднее время выполнения',time_task,'часа')
