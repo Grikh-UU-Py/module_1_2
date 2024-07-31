30197ec6efe00f9226b8562e19b249a16ec1075c
Grikh-UU-Py <5186415@mail.ru>
2024-07-31 15:29:44 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,5 @@
+done_HomeWork = 12
+spent_hours = 1.5
+name_course = 'Python'
+time_task = spent_hours / done_HomeWork
+print('Курс:', name_course, ", всего задач:",done_HomeWork,', затрачено часов:',spent_hours,', среднее время выполенния',time_task,'часа')
